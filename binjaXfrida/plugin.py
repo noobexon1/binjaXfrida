76f251ec43acb4c62be84c753f31ed473c82a9b8
noobexon1 <noamf92@gmail.com>
2025-10-19 18:29:12 +0300

    Update README and plugin registration: Simplify README content and ensure newline at end of plugin.py file.

--- binjaXfrida/plugin.py
+++ binjaXfrida/plugin.py
@@ -5,8 +5,10 @@ def print_functions(bv: BinaryView):
     for f in bv.functions:
         log_info(f"[binjaXfrida] {f.name} -> {f.start}")
 
+
 def start(bv: BinaryView):
     log_info("[binjaXfrida] binjaXfrida plugin started!")
     print_functions(bv)
+
 
 PluginCommand.register("binjaXfrida", "Binja and Frida. Better together!", start)
