efb42b8477a7ef3ed03c7db64ac40d7786d0c266
noobexon1 <noamf92@gmail.com>
2025-10-24 22:25:06 +0300

    Update plugin.json to include 'ui' type and enhance action_utils.py with formatting improvements and whitespace adjustments for better readability.

--- binjaXfrida/actions/action_utils.py
+++ binjaXfrida/actions/action_utils.py
@@ -9,14 +9,16 @@ def get_module_name(bv: BinaryView) -> str:
     log_info(f"[binjaXfrida] get_module_name: {result}")
     return result
 
+
 def get_binja_image_base(bv: BinaryView) -> int:
     result = bv.start
     log_info(f"[binjaXfrida] get_binja_image_base: {hex(result)}")
     return result
 
+
 def get_function_relative_address(bv: BinaryView) -> int:
-    
     func = bv.get_functions_containing(here)[0]
+
 
 def copy_to_clipboard(data: str) -> bool:
     """Copies the given data string to the system clipboard."""
